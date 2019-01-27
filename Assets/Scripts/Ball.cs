9017279d9ecf7e39d88a69729deee1a151e2cd82
Jose Sandoval <joseasandovalp@gmail.com>
2019-01-27 11:12:55 -0500

    First Commit - Juego terminado

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public float difficultyMultiplier = 1.2f;
+    public float minXSpeed = 0.8f;
+    public float maxXSpeed = 1.2f;
+    public float minYSpeed = 0.8f;
+    public float maxYSpeed = 1.2f;
+
+    private Rigidbody2D ballRigidbody;
+    // Use this for initialization
+    void Start()
+    {
+        ballRigidbody = GetComponent<Rigidbody2D>();
+        ballRigidbody.velocity = new Vector2(
+            Random.Range(minXSpeed, maxXSpeed) * (Random.value > 0.5f ? -1 : 1),
+            Random.Range(minYSpeed, maxYSpeed) * (Random.value > 0.5f ? -1 : 1) //Operador ternario
+        );
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Limit")
+        {
+            GetComponent<AudioSource>().Play();
+            //Choque con limite de arriba
+            if (other.transform.position.y > transform.position.y && ballRigidbody.velocity.y > 0)
+            {
+                ballRigidbody.velocity = new Vector2(ballRigidbody.velocity.x, -ballRigidbody.velocity.y);
+            }
+            //Choque con limite de abajo
+            if (other.transform.position.y < transform.position.y && ballRigidbody.velocity.y < 0)
+            {
+                ballRigidbody.velocity = new Vector2(ballRigidbody.velocity.x, -ballRigidbody.velocity.y);
+            }
+        }
+        else if (other.tag == "Paddle")
+        {
+            GetComponent<AudioSource>().Play();
+            //Choque con paleta de la izquierda
+            if (other.transform.position.x < transform.position.x && ballRigidbody.velocity.x < 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    -ballRigidbody.velocity.x * difficultyMultiplier,
+                    ballRigidbody.velocity.y * difficultyMultiplier
+                );
+            }
+            //Choque con paleta de la derecha
+            if (other.transform.position.x > transform.position.x && ballRigidbody.velocity.x > 0)
+            {
+                ballRigidbody.velocity = new Vector2(
+                    -ballRigidbody.velocity.x * difficultyMultiplier,
+                    ballRigidbody.velocity.y * difficultyMultiplier
+                );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameController : MonoBehaviour
+{
+
+    public GameObject ballPrefab;
+    public Text score1Text;
+    public Text score2Text;
+    public Text ganadorText;
+    public float scoreCoordinates = 3.4f;
+
+    private Ball currentBall;
+    private int score1 = 0;
+    private int score2 = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        SpawnBall();
+    }
+
+    void SpawnBall()
+    {
+        GameObject ballInstance = Instantiate(ballPrefab, transform);
+
+        currentBall = ballInstance.GetComponent<Ball>();
+        currentBall.transform.position = Vector2.zero;
+
+        score1Text.text = score1.ToString();
+        score2Text.text = score2.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (currentBall != null)
+        {
+            if (currentBall.transform.position.x > scoreCoordinates)
+            {
+                score1++;
+                Destroy(currentBall.gameObject);
+                SpawnBall();
+            }
+            if (currentBall.transform.position.x < -scoreCoordinates)
+            {
+                score2++;
+                Destroy(currentBall.gameObject);
+                SpawnBall();
+            }
+        }
+
+        if (score1 == 5)
+        {
+            ganadorText.text = "¡GANA EL JUGADOR 1!";
+            Destroy(currentBall.gameObject);
+        }
+        else if (score2 == 5)
+        {
+            ganadorText.text = "¡GANA EL JUGADOR 2!";
+            Destroy(currentBall.gameObject);
+        }
+    }
+}
